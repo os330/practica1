f61f63f15d9cac07851472ce0246866cf419c380
Tomas Gonzalez Alvarado <tomas.gonzalez@smartin.tecnm.mx>
2024-10-18 15:08:30 -0600

    main profe tomas

---
+++ MainTomas.py
@@ -0,0 +1,12 @@
+a=10
+b=20
+c=a+b
+
+print("Hola Mundo")
+print(f"la suma de {a} + {b} = {c}")
+print("La suma de a + b es ",c)
+
+if a>b:
+    print (f"El mayor es {a}")
+else:
+    print(f"El mayor es {b}")
